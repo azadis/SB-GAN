5f2346704136bfcbeb32da48c378ccf8c87e8a58
Samaneh <sazadi@berkeley.edu>
2020-05-05 06:56:23 +0000

    download ade changed

--- SBGAN/datasets/download_ade.sh
+++ SBGAN/datasets/download_ade.sh
@@ -1,24 +1,53 @@
 #!/bin/bash -f
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+PDIR="$( dirname ${DIR} )"
 
 cd $DIR
-wget https://people.eecs.berkeley.edu/~sazadi/SBGAN/datasets/ADE_indoor.tar.gz
-tar -xzvf ADE_indoor.tar.gz
-rm ADE_indoor.tar.gz
 
-cd ADE_indoor
-mv ADE15c_indoor_img ../
-mv ADE15c_indoor_lbl ../
-mv ADE15c_indoor_val_img ../
-mv ADE15c_indoor_val_lbl ../
+wget http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip
+unzip ADEChallengeData2016.zip
+rm ADEChallengeData2016.zip
 
-mkdir ADE15c_indoor_train_im
-mkdir ADE15c_indoor_train_lbl
-mkdir ADE15c_indoor_val_im
-mkdir ADE15c_indoor_val_lbl
+mkdir ADE_indoor
+mkdir ADE_indoor/images
+mkdir ADE_indoor/annotations
+mkdir ADE_indoor/images/train
+mkdir ADE_indoor/images/val
+mkdir ADE_indoor/annotations/train
+mkdir ADE_indoor/annotations/val
 
-ln -s ${DIR}/ADE15c_indoor_img/*/* ADE15c_indoor_train_im/
-ln -s ${DIR}/ADE15c_indoor_lbl/*/* ADE15c_indoor_train_lbl/
-ln -s ${DIR}/ADE15c_indoor_val_img/*/* ADE15c_indoor_val_im/
-ln -s ${DIR}/ADE15c_indoor_val_lbl/*/* ADE15c_indoor_val_lbl/
+input="ade_indoor_im_info_train.txt"
+while IFS= read -r line
+do
+	pth="$(cut -d' ' -f1 <<<"$line")"
+	name="$(cut -d'/' -f5 <<<"$pth")"
+	ln -s ${PDIR}/${pth} ADE_indoor/images/train/${name}
+done < "$input"
+
+
+input="ade_indoor_lbl_info_train.txt"
+while IFS= read -r line
+do
+	pth="$(cut -d' ' -f1 <<<"$line")"
+	name="$(cut -d'/' -f5 <<<"$pth")"
+	ln -s ${PDIR}/${pth} ADE_indoor/annotations/train/${name}
+done < "$input"
+
+
+input="ade_indoor_im_info_val.txt"
+while IFS= read -r line
+do
+	pth="$(cut -d' ' -f1 <<<"$line")"
+	name="$(cut -d'/' -f5 <<<"$pth")"
+	ln -s ${PDIR}/${pth} ADE_indoor/images/val/${name}
+done < "$input"
+
+
+input="ade_indoor_lbl_info_val.txt"
+while IFS= read -r line
+do
+	pth="$(cut -d' ' -f1 <<<"$line")"
+	name="$(cut -d'/' -f5 <<<"$pth")"
+	ln -s ${PDIR}/${pth} ADE_indoor/annotations/val/${name}
+done < "$input"
